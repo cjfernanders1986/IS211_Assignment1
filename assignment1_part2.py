5de3ce5b93562bc0aa63535bb14975af53483ad6
Christopher Fernanders <cj.fernanders1986@gmail.com>
2016-02-03 22:42:00 -0500

    Assignment 1

---
+++ assignment1_part2.py
@@ -0,0 +1,35 @@
+#! usr/bin/env python
+# *-* coding: utf-8 *-*
+"""Week 1-Part 2"""
+
+
+class Book(object):
+    """Creates a book"""
+
+    author = ''
+    title = ''
+
+    def __init__(self, author, title):
+        """Constructor for book.
+            Args:
+                author: name of author
+                title: name of book
+            Attributes:
+                author: name of author
+                title: name of book
+        """
+        self.author = author
+        self.title = title
+
+    def display(self):
+        """Displays a book's title and author.
+        Args: None
+        Return: None
+        """
+        print "{}, written by {}".format(self.title, self.author)
+
+B1 = Book('John Steinbeck', 'Of Mice and Men')
+B2 = Book('Harper Lee', 'To Kill A Mockingbird')
+
+B1.display()
+B2.display()
